0421798c91df1a7631188788739415c89efa4e32
dimavdp <dimavdp@gmail.com>
2019-01-07 23:05:16 +0300

    First commit

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "basicscodecamp.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+// console out << "smth" << end line
+//    cout << "Jake was 35 years old" << endl;
+//    cout << "John was 50 years old" << endl;
+
+//    string characterName = "Jake";
+//    int characterAge = 30;
+//    cout << characterName << " was " << characterAge << " years old" << endl;
+//    characterName = "Nike";
+//    cout << characterName << " was " << characterAge << " years old" << endl;
+//    cout << "next line\n";
+
+//    char grade = 'A'; //for single character
+//    string phrase = "basicsCodeCamp"; //for plain text
+//    int age = 50; //just a solid number, without .
+//    double gpa = 2.34532; //can store more decimal numbers after . than float
+//    bool isMale = true; //for true/false situations
+
+    string phrase = "basicsCodeCamp\n";
+    cout << phrase;
+
+
+
+    return 0 ;
+}
